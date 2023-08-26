7e1aa0ef816fe2f2ff9e857b3c289f26d131a747
Nataniel Junior <fnatanieljunior@gmail.com>
2023-08-26 18:23:39 -0300

    projeto iniciado

--- Recipe/src/App.jsx
+++ Recipe/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <div>
-
+      <h1>Classic Cheesecake Recipe</h1>
+      <p>Look no further for a creamy and ultra smooth classic cheesecake recipe! Paired with a buttery graham cracker crust, no one can deny its simple decadence. For the best results, bake in a water bath.</p>
     </div>
      
   )
