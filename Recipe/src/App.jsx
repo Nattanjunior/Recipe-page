d833161cf98db244c847ecdaf0db9036ebd1a19f
Nataniel Junior <fnatanieljunior@gmail.com>
2023-08-27 18:26:02 -0300

    inciando a estilização.

--- Recipe/src/App.jsx
+++ Recipe/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       </header>
       <main>
 
-      <img src={photo} alt="Cheesecake" />
+      <img src={photo} alt="Cheesecake" id='cheese'/>
 
 
       <section id='time'>
@@ -59,25 +59,24 @@ function App() {
         <h3>Graham Cracker Crust</h3>
 
        <p><input type="checkbox" name="1" id="1" /> 
-        <label htmlFor="1">1 and 1/2 cups (150g) graham cracker crumbs (about 10 full sheet graham crackers)</label></p>
+        <label htmlFor="1">1 and 1/2 cups (150g) <strong>graham cracker crumbs</strong> (about 10 full sheet graham crackers)</label></p>
 
         <p>
           <input type="checkbox" name="2" id="2" />
-          <label htmlFor="2">5 Tablespoons (70g) unsalted butter, melted</label>
+          <label htmlFor="2">5 Tablespoons (70g) <strong>unsalted butter</strong>, melted</label>
         </p>
 
         <p>
           <input type="checkbox" name="3" id="3" />
-          <label htmlFor="3">1/4 cup (50g) granulated sugar</label>
+          <label htmlFor="3">1/4 cup (50g) <strong>granulated sugar</strong></label>
         </p>
         
 
         <h3>Cheesecake</h3>
         <p>
           <input type="checkbox" name="4" id="4" />
-          <label htmlFor="4">four 8-ounce blocks (904g) full-fat cream cheese, softened to room temperature</label>
+          <label htmlFor="4">four 8-ounce blocks (904g) full-fat <strong>cream cheese</strong>, softened to room temperature</label>
         </p>
-
 
         <p>
           <input type="checkbox" name="5" id="5" />
@@ -86,22 +85,22 @@ function App() {
 
         <p>
           <input type="checkbox" name="6" id="6" />
-          <label htmlFor="6">1 cup (240g) full-fat sour cream, at room temperature</label>
+          <label htmlFor="6">1 cup (240g) full-fat <strong>sour cream</strong>, at room temperature</label>
         </p>
 
         <p>
           <input type="checkbox" name="7" id="7" />
-          <label htmlFor="7">1 teaspoon pure vanilla extract</label>
+          <label htmlFor="7">1 teaspoon <strong>pure vanilla extract</strong></label>
         </p>
 
         <p>
           <input type="checkbox" name="8" id="8" />
-          <label htmlFor="8">2 teaspoons fresh lemon juice (optional, but recommended)</label>
+          <label htmlFor="8">2 teaspoons <strong>fresh lemon juice</strong> (optional, but recommended)</label>
         </p>
 
         <p>
           <input type="checkbox" name="9" id="9" />
-          <label htmlFor="9">3 large eggs, at room temperature</label>
+          <label htmlFor="9">3 large <strong>eggs</strong>, at room temperature</label>
         </p>
 
         <p>
@@ -117,11 +116,11 @@ function App() {
 
         <p>1 Adjust the oven rack to the lower-middle position and preheat oven to 350°F (177°C).</p>
 
-        <p>2 Make the crust: Using a food processor, pulse the graham crackers into crumbs. Pour into a medium bowl and stir in sugar and melted butter until combined. (You can also pulse it all together in the food processor.) Mixture will be sandy. Press firmly into the bottom and slightly up the sides of a 9-inch or 10-inch springform pan. No need to grease the pan first. I use the bottom of a measuring cup to pack the crust down tightly. Pre-bake for 8 minutes. Remove from the oven and place the hot pan on a large piece of aluminum foil. The foil will wrap around the pan for the water bath in step 4. Allow crust to slightly cool as you prepare the filling.</p>
+        <p>2 <strong>Make the crust</strong>: Using a food processor, pulse the graham crackers into crumbs. Pour into a medium bowl and stir in sugar and melted butter until combined. (You can also pulse it all together in the food processor.) Mixture will be sandy. Press firmly into the bottom and slightly up the sides of a 9-inch or 10-inch springform pan. No need to grease the pan first. I use the bottom of a measuring cup to pack the crust down tightly. Pre-bake for 8 minutes. Remove from the oven and place the hot pan on a large piece of aluminum foil. The foil will wrap around the pan for the water bath in step 4. Allow crust to slightly cool as you prepare the filling.</p>
 
-        <p>3 Make the filling: Using a handheld or stand mixer fitted with a paddle attachment, beat the cream cheese and granulated sugar together on medium-high speed in a large bowl until the mixture is smooth and creamy, about 2 minutes. Add the sour cream, vanilla extract, and lemon juice then beat until fully combined. On medium speed, add the eggs one at a time, beating after each addition until just blended. After the final egg is incorporated into the batter, stop mixing. To help prevent the cheesecake from deflating and cracking as it cools, avoid over-mixing the batter as best you can.</p>
+        <p>3 <strong>Make the filling</strong>: Using a handheld or stand mixer fitted with a paddle attachment, beat the cream cheese and granulated sugar together on medium-high speed in a large bowl until the mixture is smooth and creamy, about 2 minutes. Add the sour cream, vanilla extract, and lemon juice then beat until fully combined. On medium speed, add the eggs one at a time, beating after each addition until just blended. After the final egg is incorporated into the batter, stop mixing. To help prevent the cheesecake from deflating and cracking as it cools, avoid over-mixing the batter as best you can.</p>
 
-        <p>4 Prepare the simple water bath (see note) Boil a pot of water. You need 1 inch of water in your roasting pan for the water bath, so make sure you boil enough. I use an entire kettle of hot water. As the water is heating up, wrap the aluminum foil around the springform pan. Pour the cheesecake batter on top of the crust. Use a rubber spatula or spoon to smooth it into an even layer. Place the pan inside of a large roasting pan. Carefully pour the hot water inside of the pan and place in the oven. (Or you can place the roasting pan in the oven first, then pour the hot water in. Whichever is easier for you.)</p>
+        <p>4 <strong>Prepare the simple water bath (see note)</strong> Boil a pot of water. You need 1 inch of water in your roasting pan for the water bath, so make sure you boil enough. I use an entire kettle of hot water. As the water is heating up, wrap the aluminum foil around the springform pan. Pour the cheesecake batter on top of the crust. Use a rubber spatula or spoon to smooth it into an even layer. Place the pan inside of a large roasting pan. Carefully pour the hot water inside of the pan and place in the oven. (Or you can place the roasting pan in the oven first, then pour the hot water in. Whichever is easier for you.)</p>
 
         <p> 5 Bake cheesecake for 55-70 minutes or until the center is almost set. When it’s done, the center of the cheesecake will slightly wobble if you gently shake the pan. Turn the oven off and open the oven door slightly. Let the cheesecake sit in the oven in the water bath as it cools down for 1 hour. Remove from the oven and water bath, then cool cheesecake completely at room temperature. Then refrigerate the cheesecake for at least 4 hours or overnight.</p>
 
